237c8d49916cbf6f4c9b25a42d383c3517a26fd4
Chahwan <skychahwan@gmail.com>
2025-10-25 16:11:51 +0900

    updated to support UCX and OpenMPI

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 
 docker build -t dms:latest -f /Users/mason/DMS/docker/Dockerfile /Users/mason/DMS/docker --no-cache
 
-docker run --rm -dit --name dms --privileged --network host --ipc host -v /Users/mason/DMS:/workspace/DMS dms:latest bash
+docker run --rm -dit --name dms --privileged --network host --ipc host --shm-size=1g -v /Users/mason/DMS:/workspace/DMS dms:latest bash
